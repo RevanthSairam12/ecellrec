4588c69a4e6ab8d76175785a861c082b3176ef07
pavan <124360829+BhimaPavanTeja@users.noreply.github.com>
2025-01-01 16:11:32 +0530

    Update page.tsx

--- app/joinUsForm/result/page.tsx
+++ app/joinUsForm/result/page.tsx
@@ -1,15 +1,15 @@
-
+import RetroGrid from '@/components/ui/retro-grid';
 const Result = () => {
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-50">
-      <h1 className="text-2xl font-bold text-green-600 mb-4">
-        Thank you for registering!
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      <h1 className="text-2xl font-bold text-gray-700 mb-4">
+        Thank you for being a member of ecell rec!
       </h1>
       <p className="text-lg text-gray-700 mb-6">
-        We&apos;ll send you a confirmation email by tomorrow, once we verify
-        your payment status.
+        you&apos;ll get news about exclusive events, workshops, and resources to help you start your entrepreneurial journey in ECELL REC.
       </p>
+      <RetroGrid />
     </div>
   );
 };
