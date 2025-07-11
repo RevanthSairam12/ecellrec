035a3613686c917a023cbdf25f675eb1c21a7428
RevanthSairam12 <revanth242a@gmail.com>
2025-07-11 21:15:30 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import Image from "next/image";
 import EcellNew from "./images/ecellverynew.png";
 import IICLogo from "./images/iic.png";
