e37e4b38cd4495dee1dc4f3f2784defb1d25c261
Eddipilli-jeevan05 <jeevan.eddipilli@gmail.com>
2025-07-18 00:17:29 +0530

    Create CurrentTeamData.ts

---
+++ app/team-cmp/CurrentTeamData.ts
@@ -0,0 +1,154 @@
+
+
+
+const CurrentTeamMembers = [{
+    role: "Overall Head",
+    name: "Sriram Vishal Epu",
+    socialLinks: [
+        { platform: 'Twitter', url: 'https://x.com/vishale1820?s=21&t=cirn4fQ-h3qu7URsa060cg' },
+        { platform: 'LinkedIn', url: 'https://www.linkedin.com/in/vishal-epu-646822222/' },
+    ],
+    imageUrl: "/vishal.jpg",
+}, {
+    role: "Vice President",
+    name: "I.Sree Venkat Nadh",
+    socialLinks: [
+        { platform: 'Twitter', url: '#' },
+        { platform: 'LinkedIn', url: 'https://www.linkedin.com/in/sree-venkatanadh-677bbb31b/' },
+    ],
+    imageUrl: "/Team/venky.jpg",
+}, {
+    role: "Vice President",
+    name: "Aamuktha Malyadha",
+    socialLinks: [
+        { platform: 'Twitter', url: 'https://x.com/aamuktha89755' },
+        { platform: 'LinkedIn', url: 'https://www.linkedin.com/in/aamuktha-malyadha-b29659316/' },
+    ],
+    imageUrl: "/aamukta.jpg",
+}, {
+    role: "Startup Assistance & program Head",
+    name: "G.V Santosh",
+    socialLinks: [
+        { platform: 'Twitter', url: 'https://x.com/SantoshGv173485' },
+        { platform: 'LinkedIn', url: 'https://www.linkedin.com/in/santosh-gv-545021293' },
+    ],
+    imageUrl: "/Team/santosh.jpg",
+}, {
+    role: "Startup Assistance & program Head",
+    name: "Tappiti Ganesh",
+    socialLinks: [
+        { platform: 'Twitter', url: 'https://x.com/ganeshtappiti' },
+        { platform: 'LinkedIn', url: 'https://www.linkedin.com/in/ganeshtappiti/' },
+    ],
+    imageUrl: "/ganesh.jpg",
+}, {
+    role: "Startup Assistance & program Head",
+    name: "Ponugupati Sai Pawan",
+    socialLinks: [
+        { platform: 'Twitter', url: 'https://x.com/pawan28016' },
+        { platform: 'LinkedIn', url: 'https://www.linkedin.com/in/ponugupati-sai-pawan/' },
+    ],
+    imageUrl: "/Team/pavan.jpg",
+}, {
+    role: "Alumni & Community Relations Head",
+    name: "Ujjesha Nitya Routhu",
+    socialLinks: [
+        { platform: 'Twitter', url: 'https://x.com/UjjeshaNitya' },
+        { platform: 'LinkedIn', url: 'https://www.linkedin.com/in/ujjesha-nitya-routhu-5a4938312/' },
+    ],
+    imageUrl: "/Team/nitya.jpg",
+}, {
+    role: "Web Tech Head",
+    name: "Jeevan Sai Eddipilli",
+    socialLinks: [
+        { platform: 'Twitter', url: 'https://x.com/__Jeevan_sai__' },
+        { platform: 'LinkedIn', url: 'https://www.linkedin.com/in/jeevan-sai-eddipilli-43734a329/' },
+    ],
+    imageUrl: "/Team/EJeevan.jpg",
+}, {
+    role: "Web Tech Head",
+    name: "Revanth Sai Ram",
+    socialLinks: [
+        { platform: 'Twitter', url: '#' },
+        { platform: 'LinkedIn', url: 'https://www.linkedin.com/in/revanth-gudimetla-9a2565280/' },
+    ],
+    imageUrl: "/placeholder.png",
+}, {
+    role: "Designing & Branding Head",
+    name: "Sai Sashank Nath",
+    socialLinks: [
+        { platform: 'Twitter', url: '#' },
+        { platform: 'LinkedIn', url: '#' },
+    ],
+    imageUrl: "/Team/sashank.jpg"
+}, {
+    role: "Marketing & Outreach Head",
+    name: "Vineela Vadrevu",
+    socialLinks: [
+        { platform: 'Twitter', url: 'https://x.com/VVadrevu39607' },
+        { platform: 'LinkedIn', url: 'https://www.linkedin.com/in/sai-satya-vineela-vadrevu-976b04374/' },
+    ],
+    imageUrl: "/Team/vineela.jpg",
+}, {
+    role: "Marketing & Outreach Head",
+    name: "Sagi Sanvitha",
+    socialLinks: [
+        { platform: 'Twitter', url: '#' },
+        { platform: 'LinkedIn', url: '#' },
+    ],
+    imageUrl: "/Team/sanvitha.jpg",
+}, {
+    role: "Events & Management Head",
+    name: "Mohammad Shahidulla",
+    socialLinks: [
+        { platform: 'Twitter', url: '#' },
+        { platform: 'LinkedIn', url: 'https://www.linkedin.com/in/mohammad-shahidulla-214428321/' },
+    ],
+    imageUrl: "/Team/shahid.jpg",
+}, {
+    role: "Events & Management Head",
+    name: "Shazia sadaf",
+    socialLinks: [
+        { platform: 'Twitter', url: '#' },
+        { platform: 'LinkedIn', url: 'https://www.linkedin.com/in/shazia-sadaf-83837933a/' },
+    ],
+    imageUrl: "/Team/shazia.jpg",
+}, {
+    role: "Events & Management Head",
+    name: "Enduga Ramya",
+    socialLinks: [
+        { platform: 'Twitter', url: '#' },
+        { platform: 'LinkedIn', url: '#' },
+    ],
+    imageUrl: "/Team/ramya.jpg",
+}, {
+    role: "Events & Management Head",
+    name: "Jeevan Kurmadasu",
+    socialLinks: [
+        { platform: 'Twitter', url: '#' },
+        { platform: 'LinkedIn', url: 'https://www.linkedin.com/in/kurmadasu-jeevan-a54903337/' },
+    ],
+    imageUrl: "/Team/KJeevan.jpg",
+}, {
+    role: "Club Chronicles Head",
+    name: "Renitha Ashwa",
+    socialLinks: [
+        { platform: 'Twitter', url: '#' },
+        { platform: 'LinkedIn', url: '#' },
+    ],
+    imageUrl: "/Team/renitha.jpeg",
+}, {
+    role: "Club Chronicles Head",
+    name: "shaik karishma",
+    socialLinks: [
+        { platform: 'Twitter', url: '#' },
+        { platform: 'LinkedIn', url: '#' },
+    ],
+    imageUrl: "/Team/karishma.jpg",
+}
+
+]
+
+
+
+export default CurrentTeamMembers;
